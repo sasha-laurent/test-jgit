962c4ee4a7f21558c92a84936eaf8ee6fe4f4d34
Sasha Laurent <sasha.laurent-stagiaire@laposte.fr>
2017-03-05 17:16:18 +0100

    retirer commentaires de bonjour

--- Bonjour.java
+++ Bonjour.java
@@ -1,7 +1,5 @@
 public class Bonjour(){
   public static void main(String[] args){
-	  // Voilà comment on dit bonjour!
-	  // Ce commentaire a été modifié par un collaborateur
     System.out.println("Bonjour amélioré!");
   }
 }
