ddc4a34a4b77758dc7ef9accc9e4122db5010de8
Sasha Laurent <sasha.laurent-stagiaire@laposte.fr>
2017-03-05 13:07:43 +0100

    Commit simple après add

--- Bonjour.java
+++ Bonjour.java
@@ -1,6 +1,7 @@
 public class Bonjour(){
   public static void main(String[] args){
 	  // Voilà comment on dit bonjour!
+	  // Ce commentaire a été ajouté par un collaborateur
     System.out.println("Bonjour!");
   }
 }
